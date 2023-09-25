76a1f551e5e1c9ed5406d95dd28db0497e311cfe
Tian <yu_tian99@hotmail.com>
2023-09-24 23:35:54 -0400

    init ml model folder

---
+++ albumy/ml_models/alt_text.py
@@ -0,0 +1,5 @@
+# Add data.py in the credentials directory, and create an variable called KEY,
+# another called  END_POINT.
+# ** DO NOT COMMIT THIS CHANGE TO GIT!!!!!!!!!! **
+from credentials.data import KEY, END_POINT
+
